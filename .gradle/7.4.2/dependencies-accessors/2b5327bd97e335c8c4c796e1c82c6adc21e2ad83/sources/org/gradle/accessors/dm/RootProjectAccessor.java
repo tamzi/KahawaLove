f3af21d2e0b28971dda5d23c1994ab9962ab3c56
tamzi <tamrefrank@gmail.com>
2022-08-29 13:58:43 +0300

    refurbished update

---
+++ .gradle/7.4.2/dependencies-accessors/2b5327bd97e335c8c4c796e1c82c6adc21e2ad83/sources/org/gradle/accessors/dm/RootProjectAccessor.java
@@ -0,0 +1,56 @@
+package org.gradle.accessors.dm;
+
+import org.gradle.api.NonNullApi;
+import org.gradle.api.artifacts.ProjectDependency;
+import org.gradle.api.internal.artifacts.dependencies.ProjectDependencyInternal;
+import org.gradle.api.internal.artifacts.DefaultProjectDependencyFactory;
+import org.gradle.api.internal.artifacts.dsl.dependencies.ProjectFinder;
+import org.gradle.api.internal.catalog.DelegatingProjectDependency;
+import org.gradle.api.internal.catalog.TypeSafeProjectDependencyFactory;
+import javax.inject.Inject;
+
+@NonNullApi
+public class RootProjectAccessor extends TypeSafeProjectDependencyFactory {
+
+
+    @Inject
+    public RootProjectAccessor(DefaultProjectDependencyFactory factory, ProjectFinder finder) {
+        super(factory, finder);
+    }
+
+    /**
+     * Creates a project dependency on the project at path ":"
+     */
+    public KahawaLoveProjectDependency getKahawaLove() { return new KahawaLoveProjectDependency(getFactory(), create(":")); }
+
+    /**
+     * Creates a project dependency on the project at path ":app"
+     */
+    public AppProjectDependency getApp() { return new AppProjectDependency(getFactory(), create(":app")); }
+
+    /**
+     * Creates a project dependency on the project at path ":benchmark"
+     */
+    public BenchmarkProjectDependency getBenchmark() { return new BenchmarkProjectDependency(getFactory(), create(":benchmark")); }
+
+    /**
+     * Creates a project dependency on the project at path ":kahawaLovebenchmark"
+     */
+    public KahawaLovebenchmarkProjectDependency getKahawaLovebenchmark() { return new KahawaLovebenchmarkProjectDependency(getFactory(), create(":kahawaLovebenchmark")); }
+
+    /**
+     * Creates a project dependency on the project at path ":mkahawa"
+     */
+    public MkahawaProjectDependency getMkahawa() { return new MkahawaProjectDependency(getFactory(), create(":mkahawa")); }
+
+    /**
+     * Creates a project dependency on the project at path ":mkahawademo"
+     */
+    public MkahawademoProjectDependency getMkahawademo() { return new MkahawademoProjectDependency(getFactory(), create(":mkahawademo")); }
+
+    /**
+     * Creates a project dependency on the project at path ":mkahawaemoBenchmark"
+     */
+    public MkahawaemoBenchmarkProjectDependency getMkahawaemoBenchmark() { return new MkahawaemoBenchmarkProjectDependency(getFactory(), create(":mkahawaemoBenchmark")); }
+
+}
---
+++ app/src/main/java/com/tamzi/kahawalove/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.tamzi.kahawalove
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.tamzi.kahawalove.ui.theme.KahawaLoveTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            KahawaLoveTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Kahawa Love")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    KahawaLoveTheme {
+        Greeting("Android")
+    }
+}

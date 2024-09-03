fa7f4455b197ec5e147b3f83082671425bf73917
tamzi <tamrefrank@gmail.com>
2024-09-03 23:19:27 +0200

    depend on mkahwa as min theme

--- mkahawa/src/main/java/com/tamzi/mkahawa/Theme.kt
+++ mkahawa/src/main/java/com/tamzi/mkahawa/Theme.kt
@@ -1,6 +1,7 @@
-package com.tamzi.kahawalove.ui.theme
+package com.tamzi.mkahawa
 
-import android.app.Activity
+
+
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -10,6 +11,8 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
+import com.tamzi.mkahawa.atoms.color.*
+import com.tamzi.mkahawa.atoms.type.Typography
 
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
@@ -34,7 +37,7 @@ private val LightColorScheme = lightColorScheme(
 )
 
 @Composable
-fun KahawaLoveTheme(
+fun MkahawaTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
--- mkahawademo/src/main/java/com/tamzi/mkahawademo/MkahawaDemoMainActivity.kt
+++ mkahawademo/src/main/java/com/tamzi/mkahawademo/MkahawaDemoMainActivity.kt
@@ -11,14 +11,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.tamzi.mkahawademo.ui.theme.KahawaLoveTheme
+import com.tamzi.mkahawa.MkahawaTheme
 
 class MkahawaDemoMainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            KahawaLoveTheme {
+            MkahawaTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
                         name = "Android",
@@ -41,7 +41,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    KahawaLoveTheme {
+    MkahawaTheme {
         Greeting("Android")
     }
 }

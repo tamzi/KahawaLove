febdc4c04040204b908489e39472621789d638b4
tamzi <tamrefrank@gmail.com>
2024-09-04 22:32:26 +0200

    Adding color and font ideas

--- mkahawa/src/main/java/com/kahawalove/mkahawa/atoms/color/Color.kt
+++ mkahawa/src/main/java/com/kahawalove/mkahawa/atoms/color/Color.kt
@@ -2,10 +2,103 @@ package com.kahawalove.mkahawa.atoms.color
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+/** Note on the color scheme:
+ * All app backgrounds will be KLWarmNeutral
+ *
+ * The shades colours are for the seasons that we are in.
+ * The idea is to have a base color and then have shades of that color depending on the season.
+ * The base color will be the main color of the season.
+ * The shades will be used for the different elements in the app.
+ *
+ *
+ * NOTE: There is NO light and dark mode quite yet.
+ *
+*/
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+
+/** Brand colors*/
+val KLGreen = Color(0xFF006241)
+val KLAccentGreen = Color(0xFF00704A)
+val KLLightGreen = Color(0xFFD4E9E2)
+val KLHouseGreen = Color(0xFF1E3932)
+val KLBlack = Color(0xFF000000)
+val KLLightBlack = Color(0xFF22292F)
+
+val KLWarmNeutral = Color(0xFFF2F0EB)
+val KLCoolNeutral = Color(0xFFF9F9F9)
+val KLWhite = Color(0xFFFFFFFF)
+
+
+/** Shades of Spring:
+ *
+ * to this scheme we have a base of already existing:
+ *  - `KLGreen`,
+ *  - `KLHouseGreen`,
+ *  - `KLLightGreen`,
+ *
+ * */
+val KLSpringYellow = Color(0xFFEED350)
+val KLLightBlue = Color(0xFFB6CBD5)
+val KLSprCoral = Color(0xFFD3705A)
+val KLTeal = Color(0xFF005E66)
+val KLTurquoise = Color(0xFF4C9F87)
+val KLGoldenrod = Color(0xFFC89543)
+val KLSpringGreen = Color(0xFFC0D48B)
+val KLBlush = Color(0xFFEDC4C9)
+
+/** Shades of Summer:
+ *
+ * to this scheme we have a base of already existing:
+ *  - `KLGreen`,
+ *  - `KLHouseGreen`,
+ *  - `KLLightGreen`,
+ *
+ * */
+val KLSummerMauve = Color(0xFFDD92C0)
+val KLPeach = Color(0xFFf0CDBC)
+val KLSummerYellow = Color(0xFFf0E0B0)
+val KLSoftPink = Color(0xFFf0D4DC)
+val KLBrightGreen = Color(0xFF4CA06F)
+val KLVibrantOrange = Color(0xFFDC714E)
+
+
+/** Shades of Nitro:
+ *
+ * to this scheme we have a base of already existing:
+ *  - `KLGreen`,
+ *  - `KLHouseGreen`,
+ *  - `KLLightGreen`,
+ *  - `KLBlack`,
+ *
+ * */
+val KLBrickRed = Color(0xFFFF6740)
+val KLNitroGreen = Color(0xFF3ECEA9)
+val KLNitroCoral = Color(0xFFE76069)
+val KLBrightPurple = Color(0xFF8882F4)
+
+
+/** Shades of Fall:
+ *
+ * to this scheme we have a base of already existing:
+ *  - `KLGreen`,
+ *  - `KLHouseGreen`,
+ *  - `KLLightGreen`,
+ *  - `KLBlack`,
+ *
+ * */
+val KLRust = Color(0xFF9D5116)
+val KLNeutralPink = Color(0xFFEBCABC)
+val KLAlmostPumpkin = Color(0xFFE44C2C)
+val KLMustard = Color(0xFFA17700)
+val KLBurgandy = Color(0xFF8882F4)
+
+
+
+
+
+//Others
+val KLBrown = Color(0xFF9D5217)
+val KLOrange = Color(0xFFC99543)
+val KLLightOrange = Color(0xFFDBB781)
+val KLGrey = Color(0xFFE4DFD7)
+val KLLightGrey = Color(0xFFF4F0E3)
---
+++ mkahawa/src/main/java/com/kahawalove/mkahawa/atoms/type/KLFont.kt
@@ -0,0 +1,24 @@
+package com.kahawalove.mkahawa.atoms.type
+
+/**
+ *
+ * a. Sodo Sans font family:
+ *  - This is our most versatile typeface, frequently used for body copy.
+ *  - Weight: Thin, Light, Regular, SemiBold, Bold
+ *  - Size: Small, Medium, Large
+ *
+ * b. Lander font family:
+ *  - Lander is the serif typeface used as an accent for more expressive moments.
+ *  - Weight: Thin, Ligt, Regular, SemiBold, Bold
+ *  - Size: Small, Medium, Large
+ *
+ * c. Pike font family:
+ *  - is the impactful typeface.
+ *  - its condensed appearance can lend itself well to functional headlines and wayfinding.
+ *  - Weight: Light, regular & Bold
+ *  - Size: Small, Medium, Large
+ *
+ *  d. Poppins font family: TBD
+ *
+ * */
+
